e5363068de79393ce3f261b519f76ccb448bfb5a
mattthu <matthias.kurs@khk.ee>
2018-01-30 11:04:05 +0200

    muutujate defineerimine

--- praks1/valjund.php
+++ praks1/valjund.php
@@ -15,3 +15,8 @@
 //Väljastamine
 echo "Hello World!"."<br />"; // Käsu lõpp - ;
 print "valikaine<br />";
+// muutujate loomine
+$eesnimi = "Matthias"; // eesnimi, string
+$perenimi = "Kurs"; // perenimi, string
+$bussiNumber = 4; // bussi number, integer
+$soiduKestvus = 0.8; // sõidu kestvus, 0.8 tundi, float
