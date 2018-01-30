dd08698ca93eabbb1e4b3f9c57bf6538da9d4c3e
mattthu <matthias.kurs@khk.ee>
2018-01-30 11:39:58 +0200

    Arvutuste loomine

--- praks1/soogikalkulaator.php
+++ praks1/soogikalkulaator.php
@@ -6,6 +6,14 @@
  * Time: 11:28
  */
 // Vajalikud muutujad
-$soogiHInd = 2.55; // hind, EUR
+$soogiHind = 2.55; // hind, EUR
 $soodusProtsent = 15; // %
 $opilaseToetus = 1.80; // EUR
+// arvutused
+if($soogiHind > 0 and $soodusProtsent > 0 and $opilaseToetus > 0) {
+    $soodusHind = $soogiHind * ((100 - $soodusProtsent) / 100);
+    $hindOpilasele = $soodusHind - $opilaseToetus;
+    echo "Söögi hind õpilasele on ".$hindOpilasele."<br />";
+} else {
+    echo "Andmed ei ole täielikud<br />";
+}
