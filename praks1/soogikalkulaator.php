9bce4ba64315f685678b48f5a6304650066316cd
mattthu <matthias.kurs@khk.ee>
2018-02-14 11:50:36 +0200

    Uus fail funktsioonidega

--- praks1/soogikalkulaator.php
+++ praks1/soogikalkulaator.php
@@ -17,3 +17,23 @@ if($soogiHind > 0 and $soodusProtsent > 0 and $opilaseToetus > 0) {
 } else {
     echo "Andmed ei ole täielikud<br />";
 }
+// funktsioon soodustuse arvutamiseks
+/**
+ * @param $taisHind
+ * @param $soodusKaart
+ * @param $kasOledOpilane
+ */
+function soogiHind ($taisHind, $soodusKaart, $kasOledOpilane) {
+    // funktsiooni sisu
+    $toetus = 1.80; // õpilase toetus eurodes
+    $soodustusProtsent = 15; // soodustusprotsent
+    $soodusHind = $taisHind;
+
+    if ($soodusKaart) {
+        $soodusHind = $taisHind - ($taisHind * ($soodustusProtsent / 100));
+    }
+    if ($kasOledOpilane) {
+        $soodusHind -= $toetus;
+    }
+    return $soodusHind;
+} // funktsiooni lõpp
