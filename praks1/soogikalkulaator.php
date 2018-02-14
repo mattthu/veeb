b1c8dfa45b9f25a47b81639ac26bb574b5304bd1
mattthu <matthias.kurs@khk.ee>
2018-02-14 11:55:22 +0200

    Funktsioonide üleviimine teise faili. funktsioonide faili importimine ja html faili lugemine.

--- praks1/soogikalkulaator.php
+++ praks1/soogikalkulaator.php
@@ -5,35 +5,8 @@
  * Date: 30.01.2018
  * Time: 11:28
  */
-// Vajalikud muutujad
-$soogiHind = 2.55; // hind, EUR
-$soodusProtsent = 15; // %
-$opilaseToetus = 1.80; // EUR
-// arvutused
-if($soogiHind > 0 and $soodusProtsent > 0 and $opilaseToetus > 0) {
-    $soodusHind = $soogiHind * ((100 - $soodusProtsent) / 100);
-    $hindOpilasele = $soodusHind - $opilaseToetus;
-    echo "Söögi hind õpilasele on ".$hindOpilasele."<br />";
-} else {
-    echo "Andmed ei ole täielikud<br />";
-}
-// funktsioon soodustuse arvutamiseks
-/**
- * @param $taisHind
- * @param $soodusKaart
- * @param $kasOledOpilane
- */
-function soogiHind ($taisHind, $soodusKaart, $kasOledOpilane) {
-    // funktsiooni sisu
-    $toetus = 1.80; // õpilase toetus eurodes
-    $soodustusProtsent = 15; // soodustusprotsent
-    $soodusHind = $taisHind;
 
-    if ($soodusKaart) {
-        $soodusHind = $taisHind - ($taisHind * ($soodustusProtsent / 100));
-    }
-    if ($kasOledOpilane) {
-        $soodusHind -= $toetus;
-    }
-    return $soodusHind;
-} // funktsiooni lõpp
+require_once 'funktsioonid.php'; // ligipääs funktsioonid.php failile
+
+// väljastame html vormi
+loeVormFailist('vorm.html');
