8006c8b952b816644ea2ce7d39c9be40b0562347
mattthu <matthias.kurs@khk.ee>
2018-05-20 14:01:35 +0300

    Muudetud ühenduse loomet ning lisatud saatmise ja andmete väljastamise

--- praks3/ab_fnk.php
+++ praks3/ab_fnk.php
@@ -5,17 +5,56 @@
  * Date: 19.05.2018
  * Time: 17:03
  */
-require_once 'ab_konf.php';
-// ühendus ab serveriga
-function yhendus(){
-    $ab_yhendus = mysqli_connect(AB_HOST, AB_USER, AB_PASS, AB_NIMI);
-    if($ab_yhendus == false){
-        echo 'Probleem andmebaasi ühendamisega<br />';
-        echo mysqli_connect_error().'<br />';
-        echo mysqli_connect_errno().'<br />';
-        exit;
-    } else {
-        echo 'Ühendus on loodud<br />';
+$servername = "localhost";
+$username = "kursmatthiasiktk";
+$password = "TugeVParoolMIdaKeegieiTea7989";
+$dbname = "kursmatt_test";
+$eesnimi = $_POST["eesnimi"];
+$perenimi = $_POST["perenimi"];
+$aeg = $_POST["aasta"]."-".$_POST["kuu"]."-".$_POST["paev"];
+$conn = new mysqli($servername, $username, $password, $dbname);
+function uhendus()
+{
+    global $servername, $username, $password, $dbname, $conn;
+    #Loo ühendus
+    $conn = new mysqli($servername, $username, $password, $dbname);
+    #Kontrolli ühendust
+    if ($conn->connect_error) {
+        die("<br>Ühendus puudub" . $conn->connect_error);
     }
-    return $ab_yhendus;
+    return $conn;
+}
+function send() {
+    global $eesnimi,$perenimi, $aeg, $servername, $username, $password, $dbname, $conn;
+    #Andmete saatmine
+    $con=mysqli_connect($servername,$username,$password,$dbname);
+    $val = mysqli_query($con,"select * from andmed WHERE eesnimi='$eesnimi' AND perenimi='$perenimi'");
+    if (!$val) {
+        die('Error: ' . mysqli_error($con));
+    }
+    if(mysqli_num_rows($val) > 0) {
+        echo "See isik on juba andmed sisestanud!<br><br>";
+    } else {
+        $sql = "INSERT INTO andmed (eesnimi, perenimi, aeg) VALUES ('$eesnimi', '$perenimi', '$aeg')";
+        #Kontrollin kas andmed on saadetud
+        if ($conn->query($sql) === TRUE) {
+            echo "Andmed edastatud!<br><br>";
+        } else {
+            echo "Error: " . $sql . "<br>" . $conn->error;
+        }
+    }
+}
+function selectALL() {
+    global $conn;
+    $sql = "SELECT * FROM andmed";
+    $result = $conn->query($sql);
+    if ($result->num_rows > 0) {
+        # Väljastatud read
+        while ($row = $result->fetch_assoc()) {
+            echo "ID: " . $row["id"] . "  Nimi: " . $row["eesnimi"] . " " . $row["perenimi"] . " Aeg: " . $row["aeg"] . "<br>";
+        }
+    } else {
+        echo "0 results";
+        $conn->close();
+    }
 }
