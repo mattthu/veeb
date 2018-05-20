ba0d17b360862d20994818cc05c7f0758febb8da
mattthu <matthias.kurs@khk.ee>
2018-05-20 13:56:43 +0300

    HTMLI ja funktsioonide importimiseks loodud read

--- praks3/funktsioonid.php
+++ praks3/funktsioonid.php
@@ -5,3 +5,38 @@
  * Date: 20.05.2018
  * Time: 13:43
  */
+function soogiHind($taisHind, $soodusKaart = false, $kasOledOpilane = false){
+    // funktsiooni sisu
+    $soodusHind = $taisHind;
+    $soodustusProtsent = 15; // %
+    $opilaseToetus = 1.80; // EUR
+    if($soodusKaart){
+        $soodusHind = $taisHind * ((100 - $soodustusProtsent) / 100);
+    }
+    if($kasOledOpilane){
+        $soodusHind = $soodusHind - $opilaseToetus;
+        // $soodusHind -= $opilaseToetus; // - operaatori lühendatud kuju
+    }
+    return $soodusHind;
+}// funktsiooni lõpp
+// funktsioon vormi väljastamiseks
+// vorm hoiame vorm.html failis
+// vormi sisu loeme antud failist ja
+// väljastame
+function loeVormFailist($failinimi)
+{
+    // siia salvestame sisu
+    $sisu = '';
+    // kontrollime vajaliku faili olemasolu
+    if (file_exists($failinimi) and is_file($failinimi) and is_readable($failinimi)) {
+        // saab failist lugeda
+        $fp = fopen($failinimi, 'r'); // tekitame ühendus lugemiseks
+        // loeme failist täissisu
+        $sisu = fread($fp, filesize($failinimi));
+        fclose($fp); // paneme ühendus failiga kinni
+    } else {
+        echo 'Probleem ' . $failinimi . ' failiga<br />';
+        exit;
+    }
+    echo $sisu;
+}
